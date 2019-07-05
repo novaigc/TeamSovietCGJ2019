bf7b0dedd63392c89c55bed9cb7cf751c93d9020
LywockeeZ <kofln@vip.qq.com>
2019-07-05 22:56:35 +0800

    Base Movement

---
+++ Assets/Scripts/LywockeeZ/MovementController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float rotateSpeed = 1f;
+    private Vector3 direction;
+    private Vector3 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector3.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    public void Move()
+    {
+        Vector3 anyKeyDown = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0).normalized;
+        transform.position += transform.right * Time.deltaTime * moveSpeed * anyKeyDown.magnitude;
+    }
+
+
+
+    public void Rotate()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0 && Input.GetAxisRaw("Vertical") > 0)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, 0, 45), rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0 && Input.GetAxisRaw("Vertical") < 0)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, 0, -45), rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0 && Input.GetAxisRaw("Vertical") > 0)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, 0, 135), rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0 && Input.GetAxisRaw("Vertical") < 0)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, 0, -135), rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") > 0)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, 0, 90), rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") < 0)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, 0, -90), rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0 && Input.GetAxisRaw("Vertical") == 0)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, 0, 0), rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0 && Input.GetAxisRaw("Vertical") == 0)
+        {
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, 0, 180), rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+        {
+            //direction = Vector3.zero;
+        }
+    }
+}
