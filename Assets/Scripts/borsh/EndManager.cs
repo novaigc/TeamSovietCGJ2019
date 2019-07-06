57ed486761a725fc4703b6757996cd9ae29db668
boringfish <1586602728@qq.com>
2019-07-06 15:04:02 +0800

    title end

--- Assets/Scripts/borsh/EndManager.cs
+++ Assets/Scripts/borsh/EndManager.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class EndManager : MonoBehaviour
 {
+    private GameObject[] orders = new GameObject[5];
     // Start is called before the first frame update
     void Start()
     {
         GameManager.gameManager.loadorder();
+        for (int i=1;i<=5;i++)
+        {
+            GameObject.Find("order" + i.ToString()).GetComponent<Text>().text = GameManager.gameManager.order[i - 1].ToString("00000");
+        }      
     }
 
     // Update is called once per frame
